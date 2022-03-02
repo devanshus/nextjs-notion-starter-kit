0d75e20e571cfacb6d812765982ffb2316108a58
Devanshu Sharma <3.devanshu@gmail.com>
2022-03-02 18:48:13 +0530

    Customized Attributes
    Added custom attributes to make it useful

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '83438e74baae4187bbf45db9c4bd1e13',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Devanshu Sharma',
+  domain: 'devanshu.in',
+  author: 'Devanshu Sharma',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: 'Portfolio',
+  socialImageTitle: 'Dev S',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'devanshux',
+  github: 'devanshus',
+  linkedin: 'in/devanshus',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
