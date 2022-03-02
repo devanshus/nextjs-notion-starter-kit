8fa1137af53466a5f0a6c6d4698b6c479714e2c8
Devanshu Sharma <3.devanshu@gmail.com>
2022-03-02 19:01:45 +0530

    Fixed a spelling mistake

--- site.config.js
+++ site.config.js
@@ -19,7 +19,7 @@ module.exports = {
   // social usernames (optional)
   twitter: 'devanshux',
   github: 'devanshus',
-  linkedin: 'in/devanshus',
+  linkedin: 'devanshus',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
